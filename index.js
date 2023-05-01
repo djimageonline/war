de291a538add5062f3677a2b72d76f81420356fd
Johnny Proano <jproano22@yahoo.com>
2023-05-01 10:23:45 -0400

    added get card button and second fetch for cards

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ function handleClick() {
 }
 
 document.getElementById("new-deck").addEventListener("click", handleClick);
-/**
- * Challenge
- *
- * Task: Using the saved deckId, draw 2 new cards from the deck
- *
- * Docs for original Deck of Cards API: https://deckofcardsapi.com/#draw-card
- * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
- * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
- * that you'll see in the deck of cards API docs.
- */
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `;
+    });
+});
